0cd12ea68c3fbceda3d6d26413479a761b9cf98e
Jean Lucas <jeanleonino@gmail.com>
2016-04-03 22:35:33 -0400

    Try watch files

---
+++ README.md
@@ -0,0 +1,8 @@
+# Testing Gulp with ES6
+Just fooling around with gulp, babel, casperjs, and more :-)
+
+## Prerequisites
+
+* gulp
+* casperjs
+* Browserify
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,43 @@
 'use strict';
+
 var gulp = require('gulp');
+var babel = require('babelify');
+var watchify = require('watchify');
+var concat = require('gulp-concat');
+var buffer = require('vinyl-buffer');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var sourcemaps = require('gulp-sourcemaps');
 
 
-gulp.task('default', function() {
-  // place code for your default task here
-});
+function compile(watch) {
+  var bundler = watchify(browserify('./src/index.js', { debug: true }).transform(babel));
+
+  function rebundle() {
+    bundler.bundle()
+      .on('error', function(err) { console.error(err); this.emit('end'); })
+      .pipe(source('build.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('./build'));
+  }
+
+  if (watch) {
+    bundler.on('update', function() {
+      console.log('-> bundling...');
+      rebundle();
+    });
+  }
+
+  rebundle();
+}
+
+function watch() {
+  return compile(true);
+};
+
+gulp.task('build', function() { return compile(); });
+gulp.task('watch', function() { return watch(); });
+
+gulp.task('default', ['watch']);
